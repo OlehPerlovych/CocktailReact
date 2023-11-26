e08a71ebdd44998be7712c461d5b2ef484bd9fbf
OlehPerlovych <perlovich92@gmail.com>
2023-11-26 21:17:24 +0200

    Refactor to Redux, added error catching

--- src/general/navigation/Navigation.jsx
+++ src/general/navigation/Navigation.jsx
@@ -3,14 +3,12 @@ import * as Routes from './router';
 import style from './Navigation.module.css';
 import './Navigation.css';
 import {Link} from "react-router-dom";
-import {useLocation} from "react-router-dom";
+
 function Navigation(){
-    const {pathname} = useLocation();
     return (
         <div className={style.nav_container}>
             <Link className={'item_nav'} to={`/${Routes.HOME}`}>Home</Link>
             <Link className={'item_nav'} to={`/${Routes.CATEGORY}`}>Category</Link>
-            <span> Current page - {pathname}</span>
         </div>
     )
 }
